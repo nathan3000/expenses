4427de39bd7e29ca2e43658747642f721eef8c1e
Nathan Fisher <nooglefish@gmail.com>
2014-07-15 12:20:02 +0100

    Now using Guard

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
 describe "Expenses" do
-  describe "Add Expenses" do
-  	
-  	it "should have the content 'Expenses'" do
-  		visit '/expenses'
-  		expect(page).to have_content('Expenses')
-  	end
 
-  	it "should have the right title" do
-  		visit '/expenses'
-  		expect(page).to have_title('Expenses | CCS Office Apps')
-  	end
-  	
-  end
+	let(:base_title) { "| CCS Office Apps" }
+
+
+	describe "Add Expenses" do
+		
+		it "should have the content 'Expenses'" do
+			visit '/expenses'
+			expect(page).to have_content('Expenses')
+		end
+
+		it "should have the right title" do
+			visit '/expenses'
+			expect(page).to have_title("Expenses #{base_title}")
+		end
+
+	end
 end
